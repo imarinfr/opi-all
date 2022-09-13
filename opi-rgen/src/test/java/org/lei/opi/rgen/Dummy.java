06cb31709107f5bae15973d6517c98bf65c57de4
imarinfr <imarinfr@optocom.es>
2022-09-13 16:20:35 +1000

    changed the main pom

--- opi-rgen/src/test/java/org/lei/opi/rgen/Dummy.java
+++ opi-rgen/src/test/java/org/lei/opi/rgen/Dummy.java
@@ -1,5 +1,23 @@
 package org.lei.opi.rgen;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * Unitary tests for socket connections
+ *
+ * @since 0.0.1
+ */
 public class Dummy {
   
+  /**
+   *
+   * DUMMY description
+   *
+   * @since 0.0.1
+   */
+  @Test
+  public void dummyTest() {
+  }
+
 }
