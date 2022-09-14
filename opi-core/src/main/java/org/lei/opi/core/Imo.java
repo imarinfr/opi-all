f91a6f2a86cd307faae8af6001f8742ba60dae14
Andrew Turpin <aturpin@unimelb.edu.au>
2022-09-14 11:24:29 +1000

    added a few annotations in Imo

--- opi-core/src/main/java/org/lei/opi/core/Imo.java
+++ opi-core/src/main/java/org/lei/opi/core/Imo.java
@@ -21,8 +21,11 @@ public class Imo extends Jovp {
     * @return A JSON object with machine specific query information
     */
     @ReturnMsg(name = "error", desc = "Empty string for all good, else error message.")
+    @ReturnMsg(name = "msg", desc = "JSON Object with all of the other fields described in @ReturnMsg except 'error'.")
+    @ReturnMsg(name = "jovp", desc = "Any messages that the JOVP sent back.")
     public MessageProcessor.Packet query(HashMap<String, String> args) {
         String jovp = super.query(args).msg;
+
         return OpiManager.ok(String.format("{\"result\": \"Imo Queried\", \"jovp\": %s}", jovp), false);
     }
 
@@ -31,10 +34,13 @@ public class Imo extends Jovp {
     * @return A JSON object with machine specific initialise information
     */
     @Parameter(name = "ip", desc = "IP Address of the perimeter.")
-    @Parameter(name = "port", desc = "TCP port of the perimeter.")
-    @ReturnMsg(name = "error", desc = "Empty string for all good, else error message.")
+    @Parameter(name = "port", desc = "TCP port of the perimeter.", className = Double.class, min = 0, max = 65535)
+    @ReturnMsg(name = "error", desc = "Empty string for all good, else error messages from Imo.")
+    @ReturnMsg(name = "msg", desc = "JSON Object with all of the other fields described in @ReturnMsg except 'error'.")
+    @ReturnMsg(name = "jovp", desc = "Any messages that the JOVP sent back.")
     public MessageProcessor.Packet initialize(HashMap<String, String> args) {
         String jovp = super.initialize(args).msg;
+
         setIsInitialised(true);
         return OpiManager.ok(String.format("{jvop: %s}", jovp), false);
     }
@@ -46,9 +52,12 @@ public class Imo extends Jovp {
     @Parameter(name = "eye", desc = "Eye to set.", className = Eye.class)
     //@Param(name = "color", desc = "Background color for eye.", className = COLOR.class)
     //@Param(name = "fix", desc = "Fixation className for eye.", className = FIXATION.class)
-    @ReturnMsg(name = "error", desc = "Empty string for all good, else error message.")
+    @ReturnMsg(name = "error", desc = "Empty string for all good, else error messages from Imo.")
+    @ReturnMsg(name = "msg", desc = "JSON Object with all of the other fields described in @ReturnMsg except 'error'.")
+    @ReturnMsg(name = "jovp", desc = "Any messages that the JOVP sent back.")
     public MessageProcessor.Packet setup(HashMap<String, String> args) {
         String jovp = super.setup(args).msg;
+
         return OpiManager.ok(String.format("{jvop: %s}", jovp), false);
     }
 
@@ -62,15 +71,18 @@ public class Imo extends Jovp {
     @Parameter(name = "t", desc = "List of stimuli presentation times (ms).", className = Double.class, min = 0, isList = true)
     @Parameter(name = "w", desc = "List of stimuli response windows (ms).", className = Double.class, min = 0, isList = true)
     @Parameter(name = "color", desc = "List of stimuli colors.", className = Jovp.Color.class, isList = true)
+    @ReturnMsg(name = "error", desc = "Empty string for all good, else error messages from Imo.")
+    @ReturnMsg(name = "msg", desc = "JSON Object with all of the other fields described in @ReturnMsg except 'error'.")
     @ReturnMsg(name = "seen", desc = "true if seen, false if not.", className = Boolean.class)
-    @ReturnMsg(name = "error", desc = "Empty string for all good, else error message.")
     @ReturnMsg(name = "time", desc = "Response time from stimulus onset if button pressed, -1 otherwise (ms).", className = Double.class, min = -1)
     @ReturnMsg(name = "eyex", desc = "x co-ordinates of pupil at times eyet (degrees).", className = Double.class, isList = true)
     @ReturnMsg(name = "eyey", desc = "y co-ordinates of pupil at times eyet (degrees).", className = Double.class, isList = true)
     @ReturnMsg(name = "eyed", desc = "Diameter of pupil at times eyet (degrees).", className = Double.class, isList = true)
     @ReturnMsg(name = "eyet", desc = "Time of (eyex,eyey) pupil relative to stimulus onset t=0 (ms).", className = Double.class, isList = true)
+    @ReturnMsg(name = "jovp", desc = "Any messages that the JOVP sent back.")
     public MessageProcessor.Packet present(HashMap<String, String> args) {
         String jovp = super.present(args).msg;
+
         return OpiManager.ok(
             String.format("{\"x\": %s, \"y\": %s, \"eye\": %s, \"seen\": 1, \"time\": 666, \"jovp\": %s}",
                 args.get("x"), args.get("y"), args.get("eye"), jovp), false);
@@ -80,9 +92,12 @@ public class Imo extends Jovp {
      * @param args A map of name:value pairs for Params.
     * @return A JSON object with machine specific query information
     */
-    @ReturnMsg(name = "error", desc = "Empty string for all good, else error message.")
+    @ReturnMsg(name = "error", desc = "Empty string for all good, else error messages from Imo.")
+    @ReturnMsg(name = "msg", desc = "JSON Object with all of the other fields described in @ReturnMsg except 'error'.")
+    @ReturnMsg(name = "jovp", desc = "Any messages that the JOVP sent back.")
     public MessageProcessor.Packet close(HashMap<String, String> args) {
         String jovp = super.close(args).msg;
+
         setIsInitialised(false);
 
         return OpiManager.ok(String.format("{jvop: %s}", jovp), true);
