b86b4c8a26bd6a4bda42f5a70e287a7203b3012c
Andrew Turpin <aturpin@unimelb.edu.au>
2022-09-21 15:11:23 +1000

    About to restructure folder/dir for modules.... panic.

---
+++ opi-monitor/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module opi.monitor {
+    requires javafx.controls;
+    requires commons.cli;
+    requires opi.core;
+    exports org.lei.opi.monitor;
+}
---
+++ opi-monitor/src/main/java/org/lei/opi/monitor/OpiMonitor.java
@@ -0,0 +1,58 @@
+package org.lei.opi.monitor;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+
+/**
+ * JavaFX App
+ */
+public class OpiMonitor extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+
+        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "." 
+            + "\nIvan & Turps were here, Sep 2022.");
+
+        TextArea textArea = new TextArea();
+
+        var scene = new Scene(new StackPane(label, textArea), 640, 480);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("nw", false, "Do not draw GUI window");
+        options.addOption("p", true, "Port number to use on localhost");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (org.apache.commons.cli.ParseException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        if(cmd.hasOption("w")) {
+            launch();
+        }
+        else {
+            System.out.println("Starting server");
+            System.exit(0);
+        }
+    }
+}
