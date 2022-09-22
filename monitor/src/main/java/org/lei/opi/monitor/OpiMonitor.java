fc5e4277b9791d95b13c1df1a42a798c3aea600d
Andrew Turpin <aturpin@unimelb.edu.au>
2022-09-22 12:23:14 +1000

    Mocked up gui for Monitor

--- monitor/src/main/java/org/lei/opi/monitor/OpiMonitor.java
+++ monitor/src/main/java/org/lei/opi/monitor/OpiMonitor.java
@@ -4,8 +4,20 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.CommandLine;
@@ -17,20 +29,140 @@ import org.apache.commons.cli.GnuParser;
  */
 public class OpiMonitor extends Application {
 
+    /**
+     *  Presumably plot something representing the current state of 
+     * the VF measurement going on...
+     * 
+     * @param view Imageview to update
+     */
+    private void updateVFImage(ImageView view) {
+        //if (eye == "L")
+
+        InputStream stream;
+        try {
+            stream = new FileInputStream("hfa.jpg");
+        } catch (FileNotFoundException e) {
+            System.out.println("can't find vf image");
+            return;
+        }
+        Image image = new Image(stream);
+        view.setImage(image);
+        view.setFitWidth(256);
+        view.setPreserveRatio(true);
+    }
+
+    /**
+     * Get current position of eye from machine's camera...
+     *  
+     * @param eye L or R
+     * @param view ImageView to update
+     */
+    private void updateEyeImage(String eye, ImageView view) {
+        //if (eye == "L")
+
+        InputStream stream;
+        try {
+            stream = new FileInputStream("eye.jpeg");
+        } catch (FileNotFoundException e) {
+            System.out.println("can't find eye image");
+            return;
+        }
+        Image image = new Image(stream);
+        view.setImage(image);
+        view.setFitWidth(256);
+        view.setPreserveRatio(true);
+    }
+
+        // IP and Port of machine
+        // 
+    class IpPortBox {
+        GridPane box;
+        TextField ip;
+        TextField port;
+        Label lab;
+        Button testBtn;
+
+        /*
+         *    +---------+----------+--------+
+         *    |  label  |  ip      |  port  | 
+         *    |-----------------------------+
+         *    |         |   test   |        |
+         *    |         |   button |        |
+         *    +---------+----------+--------+
+         */                                       
+        IpPortBox(String label, String ip, String port) {
+            box = new GridPane();
+            box.setHgap(10);
+            box.setVgap(10);
+            //ipBox.setAlignment(Pos.CENTER_LEFT);
+            //ipBox.setPadding(new Insets(5, 20, 5, 5));
+            this.lab = new Label(label);
+            this.ip = new TextField(ip);
+            this.port = new TextField(port);
+            this.testBtn = new Button("Test Connection");
+            this.lab.setPrefWidth(150);
+            this.ip.setPrefSize(200, 20);
+            this.port.setPrefSize(100, 20);
+            this.ip.setAlignment(Pos.CENTER_LEFT);
+            this.port.setAlignment(Pos.CENTER_LEFT);
+            box.add(this.lab, 0, 0);
+            box.add(this.ip, 1, 0);
+            box.add(this.port, 2, 0);
+            box.add(this.testBtn, 1, 1);
+        }
+
+        void setIp(String s) { this.ip.setText(s); }
+        void setPort(String s) { this.port.setText(s); }
+    }
+
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
+        /*
+         *    +---------+----------+---------------+
+         *    |         |          |               |
+         *    |  left   |  right   |  Buttons for  | 
+         *    |   eye   |   eye    |    stuff      |
+         *    |         |          |               |
+         *    |---------+----------+---------------+
+         *    |   VF    |  VF      | Machine info  |
+         *    |   left  |  right   |               |
+         *    |         |          |               |
+         *    +---------+----------+---------------+
+         */                                       
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        ImageView leftEye = new ImageView();
+        ImageView rghtEye = new ImageView();
+        ImageView leftField = new ImageView();
+        ImageView rghtField = new ImageView();
+        grid.add(leftEye, 0, 0);
+        grid.add(rghtEye, 1, 0);
+        grid.add(leftField, 0, 1);
+        grid.add(rghtField, 1, 1);
 
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "." 
-            + "\nIvan & Turps were here, Sep 2022.");
+        TextArea messages = new TextArea();
+        messages.setEditable(false);
+        grid.add(messages, 2, 1);
 
-        TextArea textArea = new TextArea();
+        VBox buttonsPane = new VBox();
+        buttonsPane.setSpacing(8);
+        buttonsPane.getChildren().add(new IpPortBox("My IP and Port:", "localhost", "51234").box);
+        buttonsPane.getChildren().add(new IpPortBox("Machine IP and Port:", "localhost", "51234").box);
+        grid.add(buttonsPane, 2, 0);
 
-        var scene = new Scene(new StackPane(label, textArea), 640, 480);
+        var scene = new Scene(grid, 640*1.5, 480);
+
+        //textArea.setPrefHeight(scene.getWindow().getHeight()/2);
 
         stage.setScene(scene);
         stage.show();
+    
+        updateEyeImage("L", leftEye);
+        updateEyeImage("R", rghtEye);
+
+        updateVFImage(leftField);
+        updateVFImage(rghtField);
     }
 
     public static void main(String[] args) {
