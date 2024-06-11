b8e39fcaaee47ed1b7ae4c8e927589e2562b3a54
Andrew Turpin <aht.turpin@gmail.com>
2024-06-11 13:36:39 +0800

    delete accidently added.

--- rgen/x.r
+++
@@ -1,8 +0,0 @@
-#imageFilename = file.path("c:", "Users", "imoVifa", "Desktop", "opi3", "opi-all", "jovp", "x.jpg")
-
-imageFilename <- path.expand(file.path("~","opi3", "opi-all", "jovp", "x.jpg"))
-
-print(imageFilename)
-
-
-0.425820938823965
