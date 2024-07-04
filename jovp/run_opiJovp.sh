6834e19e9d387367702bec197449c0a195e3e1c4
Andrew Turpin <aht.turpin@gmail.com>
2024-07-04 08:21:54 +0800

    Update version. dontrun in KTPsi

--- jovp/run_opiJovp.sh
+++ jovp/run_opiJovp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CORE="$M2_HOME/repository/org/lei/opi/core/0.2.0/core-0.2.0.jar"
-OPIJ="$M2_HOME/repository/org/lei/opi/opiJovp/0.2.0/opiJovp-0.2.0.jar"
+VERSION=1.0.0
+CORE="$M2_HOME/repository/org/lei/opi/core/$VERSION/core-$VERSION.jar"
+OPIJ="$M2_HOME/repository/org/lei/opi/opiJovp/$VERSION/opiJovp-$VERSION.jar"
 JOVP="$M2_HOME/repository/es/optocom/jovp/jovp/0.1.0-SNAPSHOT/jovp-0.1.0-SNAPSHOT.jar"
 
 #mvn install -f /Users/aturpin/src/opi-all/jovp/pom.xml
@@ -28,6 +29,10 @@ CP="$CORE:
     $M2_HOME/repository/org/openjfx/javafx-base/18.0.2/javafx-base-18.0.2-mac-aarch64.jar:
     $M2_HOME/repository/org/openjfx/javafx-fxml/18.0.2/javafx-fxml-18.0.2.jar:
     $M2_HOME/repository/org/openjfx/javafx-fxml/18.0.2/javafx-fxml-18.0.2-mac-aarch64.jar:
+    $M2_HOME/repository/org/openjfx/javafx-swing/19/javafx-swing-19-mac-aarch64.jar:
+    $M2_HOME/repository/org/openjfx/javafx-swing/19/javafx-swing-19.jar:
+    $M2_HOME/repository/org/openpnp/opencv/4.9.0-0/opencv-4.9.0-0.jar:
+    $M2_HOME/repository/com/diffplug/durian/durian/2.0/durian-2.0.jar:
     $M2_HOME/repository/com/google/code/gson/gson/2.9.0/gson-2.9.0.jar:
     $M2_HOME/repository/commons-io/commons-io/2.11.0/commons-io-2.11.0.jar:
     $M2_HOME/repository/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar:
@@ -38,23 +43,17 @@ CP="$CORE:
     $M2_HOME/repository/org/lwjgl/lwjgl-shaderc/3.3.1/lwjgl-shaderc-3.3.1.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl-vulkan/3.3.1/lwjgl-vulkan-3.3.1.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl/3.3.1/lwjgl-3.3.1-natives-macos-arm64.jar:
+    $M2_HOME/repository/org/lwjgl/lwjgl-freetype/3.3.3/lwjgl-freetype-3.3.3.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl-assimp/3.3.1/lwjgl-assimp-3.3.1-natives-macos-arm64.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl-glfw/3.3.1/lwjgl-glfw-3.3.1-natives-macos-arm64.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl-stb/3.3.1/lwjgl-stb-3.3.1-natives-macos-arm64.jar:
     $M2_HOME/repository/org/lwjgl/lwjgl-shaderc/3.3.1/lwjgl-shaderc-3.3.1-natives-macos-arm64.jar:
-    $M2_HOME/repository/org/joml/joml/1.10.4/joml-1.10.4.jar:
-    $M2_HOME/repository/io/github/java-native/jssc/2.9.4/jssc-2.9.4.jar:
+    $M2_HOME/repository/org/joml/joml/1.10.5/joml-1.10.5.jar:
+    $M2_HOME/repository/io/github/java-native/jssc/2.9.6/jssc-2.9.6.jar:
+    $M2_HOME/repository/org/scijava/native-lib-loader/2.3.6/native-lib-loader-2.3.6.jar:
     $M2_HOME/repository/org/scijava/native-lib-loader/2.3.6/native-lib-loader-2.3.6.jar:
     $M2_HOME/repository/org/slf4j/slf4j-simple/2.0.3/slf4j-simple-2.0.3.jar:
-    $M2_HOME/repository/org/slf4j/slf4j-api/2.0.3/slf4j-api-2.0.3.jar:
-    $M2_HOME/repository/org/bytedeco/javacv/1.5.8/javacv-1.5.8.jar:
-    $M2_HOME/repository/org/bytedeco/javacpp/1.5.8/javacpp-1.5.8-macosx-arm64.jar:
-    $M2_HOME/repository/org/bytedeco/javacpp/1.5.8/javacpp-1.5.8.jar:
-    $M2_HOME/repository/org/bytedeco/javacpp-platform/1.5.8/javacpp-platform-1.5.8.jar:
-    $M2_HOME/repository/org/bytedeco/opencv/4.6.0-1.5.8/opencv-4.6.0-1.5.8-macosx-arm64.jar:
-    $M2_HOME/repository/org/bytedeco/opencv/4.6.0-1.5.8/opencv-4.6.0-1.5.8.jar:
-    $M2_HOME/repository/org/bytedeco/openblas/0.3.21-1.5.8/openblas-0.3.21-1.5.8-macosx-arm64.jar:
-    $M2_HOME/repository/org/bytedeco/openblas/0.3.21-1.5.8/openblas-0.3.21-1.5.8.jar
+    $M2_HOME/repository/org/slf4j/slf4j-api/2.0.3/slf4j-api-2.0.3.jar
 "
 
 CP=$(echo $CP | tr -d '\n' | tr -d ' ')
